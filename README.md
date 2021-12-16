af483ed30cbb6ee5989bcf208fbd3554be78e217
RustyPyGuy <87586467+RustyPyGuy@users.noreply.github.com>
2021-12-16 23:43:34 +0100

    Day 8 challenge 2 finally complete.
    Lots of fighting with the Rust borrow checker and conversions from
    strings to string slices, etc.

--- README.md
+++ README.md
@@ -3,45 +3,45 @@ This is my first time with the Advent of Code challenge.  This is my code for fo
 
 I'm a beginner and using this challenge to learn the language! 
 
-## Overall Features
+### Overall Features
 - launches from the command line and takes arguments for the day and an input file.  Calculates the two results.
 
-## Day 1
-### Approach and Unique features
+### Day 1
+#### Approach and Unique features
 - Proof of concept really.  The challenge was relatively easy and input was one value per line..
 
-## Day 2
-### Approach and Unique features
+### Day 2
+#### Approach and Unique features
 - Built more data structures.
 
-## Day 3
-### Approach and Unique features
+### Day 3
+#### Approach and Unique features
 
-## Day 4: Bingo Game
-### Approach and Unique features
+### Day 4: Bingo Game
+#### Approach and Unique features
 - First challenge with more complex data input with different data separators. 
 
-## Day 5: Map of Sea Vents
-### Approach and Unique features
-- For data input, used the 'filter_map()' method to deal with the different characters and convert to Rust vectors (similar to arrays in other languages).
-- Used the 'grid' crate to create a 2D addressable vector structure to map the sea vents. I intended to not use much beyond the standard library.  This was a compromise to just get sane 2D addressing without writing it myself.
+### Day 5: Map of Sea Vents
+#### Approach and Unique features
+- For data input, used the `filter_map()` method to deal with the different characters and convert to Rust vectors (similar to arrays in other languages).
+- Used the `grid` crate to create a 2D addressable vector structure to map the sea vents. I intended to not use much beyond the standard library.  This was a compromise to just get sane 2D addressing without writing it myself.
 - Challenge1: Used loops to put each integer on the grid. othing special. 
 - Challenge2: Same as Challenge 1 but with the addition of test logic to subtract index numbers for addressing since Rust can't iterate backwards without re-typing and additional methods.
 
-## Day 6: Lanternfish Growth
-### Approach and Unique features
+### Day 6: Lanternfish Growth
+#### Approach and Unique features
 - Challenge 1: Used a growing vector in the same format as the input data.  This was suitable for the small number of growth cycles in the puzzle.
 - Challenge 1: assigned values and spawned new fish with a match operation.
-- Challenge 2: Used the standard library 'collections::DeqVec' for to be able to roll the vector end to end.
+- Challenge 2: Used the standard library `collections::DeqVec` for to be able to roll the vector end to end.
 
-## Day 7: Crab Submarine Fuel Burn
-### Approach and Unique features
+### Day 7: Crab Submarine Fuel Burn
+#### Approach and Unique features
 - This challenge was more of a problem solving and math exercise rather than an intense programming challenge.
 - Challenge 1: Solved by find the median.
 - Challenge 2: Found the basic formula that describes the fuel rate.  Loop from the lowest to highest possible values to find the one that has the minimum total fuel use.
 - Used a closure in Challenge 2 to hold the fuel burn formula.
 
-## Day 8: Decoding 7 Segment Displays
-### Approach and Unique Features
+### Day 8: Decoding 7 Segment Displays
+#### Approach and Unique Features
 - The first challenge is a teaser where you just have to count how often specific patterns appear.
 - Challenge 1: Text processing and splitting followed by matches on character counts.
--- src/day8.rs
+++ src/day8.rs
@@ -3,18 +3,13 @@
 // Some imports not needed every time.
 use a_o_c::*; //import custom lib.rs module
 use std::io::Error;
+// use std::collections::HashMap;
+use bimap::BiHashMap;
 
 #[derive(Debug, Clone)]
 pub struct CodedData<'a> {
     input: Vec<&'a str>,
     output: Vec<&'a str>,
-}
-
-#[derive(Debug, Clone)]
-pub struct CodedData2<'a> {
-    input: Vec<&'a str>,
-    output: Vec<&'a str>,
-    // insert hashmap here
 }
 
 pub fn day8_challenge1(config: &Config) -> Result<i128, Error> {
@@ -33,12 +28,12 @@ pub fn day8_challenge1(config: &Config) -> Result<i128, Error> {
             output: output_half,
         });
     }
-    println!("{:?}", coded_data);
+    // println!("{:?}", coded_data);
     let mut charcount: usize = 0;
     for one_line in coded_data.iter() {
-        for character in one_line.output.iter() {
-            // The first task is easy to just count the characters.
-            match character.len() {
+        for segment in one_line.output.iter() {
+            // The first task is easy to just count the segments.
+            match segment.len() {
                 2 => charcount += 1,
                 3 => charcount += 1,
                 4 => charcount += 1,
@@ -47,41 +42,154 @@ pub fn day8_challenge1(config: &Config) -> Result<i128, Error> {
             }
         }
     }
-    println!("charcount {}", charcount);
+    // println!("charcount {}", charcount);
     Ok(charcount as i128)
 }
 
+#[derive(Debug, Clone)]
+pub struct CodedData2<'a> {
+    // input: Vec<&'a str>,
+    input: Vec<String>,
+    output: Vec<String>,
+    translation: BiHashMap<&'a str, Option<usize>>,
+    translation2: BiHashMap<&'a str, &'a str>,
+}
 pub fn day8_challenge2(config: &Config) -> Result<i128, Error> {
     // read input puzzle file
     let input_string = read_prep_puzzle_file_contents_to_string(config);
     // read called numbers all on first line and assign to a vector.
     let lines = iterate_by_lines_from_string(&input_string);
-    let mut coded_data: Vec<CodedData> = Vec::new();
+    let mut coded_data: Vec<CodedData2> = Vec::new();
     for line in lines.iter() {
         // Split the input and output on the vertical line and create vectors for each.
         let half: Vec<&str> = line.split('|').collect();
-        let input_half: Vec<&str> = half[0].split_whitespace().collect();
-        let output_half: Vec<&str> = half[1].split_whitespace().collect();
-        coded_data.push(CodedData {
-            input: input_half,
-            output: output_half,
+        // let input_half: Vec<&str> = half[0].split_whitespace().collect();
+        let input_half: String = String::from(half[0]); //NOTE: This may be the way.
+                                                        // create a vector of strings so that it can be manipulated.
+        let input_half2: Vec<String> = input_half.split(" ").map(|x| x.to_owned()).collect();
+        // .split_whitespace().collect::<Vec<String>>();
+        // let output_half: Vec<&str> = half[1].split_whitespace().collect();
+
+        let output_half: String = String::from(half[1]); //NOTE: This may be the way.
+                                                         // create a vector of strings so that it can be manipulated.
+        let output_half2: Vec<String> = output_half.split(" ").map(|x| x.to_owned()).collect();
+
+        fn sort_character_chunks3<'a>(input: &'a Vec<String>) -> Vec<String> {
+            let mut output_vec: Vec<String> = Vec::new();
+            for mut chunk in input.iter() {
+                let mut tempsort: Vec<char> = chunk.chars().collect();
+                tempsort.sort_unstable();
+                let tempsort2: String = tempsort.into_iter().collect();
+                output_vec.push(tempsort2);
+            }
+            output_vec
+        }
+
+        coded_data.push(CodedData2 {
+            input: sort_character_chunks3(&input_half2),
+            output: sort_character_chunks3(&output_half2),
+            translation: BiHashMap::with_capacity(10),
+            translation2: BiHashMap::with_capacity(10),
         });
     }
+
+    let mut total_sum: usize = 0;
     println!("{:?}", coded_data);
-    let mut charcount: usize = 0;
+    // let mut charcount: usize = 0;
     for one_line in coded_data.iter() {
-        for character in one_line.output.iter() {
-            // NOTE: TO BE MODIFIED IN NEXT REVISION. THIS IS JUST A COPY OF ABOVE.
-            match character.len() {
-                2 => charcount += 1,
-                3 => charcount += 1,
-                4 => charcount += 1,
-                7 => charcount += 1,
-                _ => (),
+        // bidirectional hash map.
+        let mut temp_translation_map2: BiHashMap<&str, &str> = BiHashMap::with_capacity(10);
+        let mut temp_translation_map3: BiHashMap<&str, &str> = BiHashMap::with_capacity(10);
+        let mut decode_string_temp: String = String::with_capacity(10);
+
+        // First pass to decode - based on length.
+        for segment in one_line.input.iter() {
+            match segment.len() {
+                2 => {
+                    temp_translation_map2.insert(segment, "1"); // ONE
+                    temp_translation_map3.insert(segment, "1");
+                }
+                3 => {
+                    temp_translation_map2.insert(segment, "7"); // SEVEN
+                    temp_translation_map3.insert(segment, "7");
+                }
+                4 => {
+                    temp_translation_map2.insert(segment, "4"); // FOUR
+                    temp_translation_map3.insert(segment, "4");
+                }
+                7 => {
+                    temp_translation_map2.insert(segment, "8"); // EIGHT
+                    temp_translation_map3.insert(segment, "8");
+                }
+                _ => {} // no action for other digits.
             }
         }
+        // closure to facilitate a comparison of character counts of a string
+        let count_compare2 = |A: &str, B: &str| {
+            let mut count: usize = 0;
+            let B_ref = &temp_translation_map3.get_by_right(B);
+            for c in A.chars() {
+                for d in B_ref.unwrap().chars() {
+                    if c == d {
+                        count += 1;
+                    }
+                }
+            }
+            count
+        };
+        for segment in one_line.input.iter() {
+            if segment.len() == 5 {
+                // TWO, THREE, FIVE have 5 segments displayed
+                if count_compare2(segment, "4") == 2 {
+                    // TWO uniquely shares only 2 segments with FOUR of characters with 5 total segments
+                    temp_translation_map2.insert(segment, "2");
+                    // println!("2 matched 2awesome!");
+                } else if count_compare2(segment, "7") == 3 {
+                    // THREE uniquely shares all three segments of SEVEN of characters with 5 total segments.
+                    temp_translation_map2.insert(segment, "3");
+                    // println!("3 matched 2awesome!");
+                } else if count_compare2(segment, "4") == 3 && count_compare2(segment, "1") == 1 {
+                    // FIVE uniquely shares 3 segments of FOUR AND only 1 segment of ONE with 5 total segments.
+                    temp_translation_map2.insert(segment, "5");
+                    // println!("5 matched 2awesome!");
+                }
+            }
+            if segment.len() == 6 {
+                // SIX, NINE, ZERO have 6 segments displayed
+                if count_compare2(segment, "1") == 1 {
+                    // SIX uniquely only shares 1 segment of ONE with 6 total segments.
+                    temp_translation_map2.insert(segment, "6");
+                    // println!("6 matched 2awesome!");
+                } else if count_compare2(segment, "7") == 3 && count_compare2(segment, "4") == 4 {
+                    // NINE uniquely shares all 3 segment of SEVEN and all 4 of FOUR with 6 total segments.
+                    temp_translation_map2.insert(segment, "9");
+                    // println!("9 matched 2wesome!");
+                } else if count_compare2(segment, "4") == 3 && count_compare2(segment, "7") == 3 {
+                    // ZERO uniquely shares only 3 segments of FOUR and all 3 segments of SEVEN with 6 total segments.
+                    temp_translation_map2.insert(segment, "0");
+                    // println!("0 matched 2wesome!");
+                }
+            }
+        }
+        // println!("hashmap {:?}", temp_translation_map);
+        println!("bihashmap {:?}", temp_translation_map2);
+        for out_segment in one_line.output.iter() {
+            // println!("out_segment {:?}", out_segment);
+
+            println!(
+                "out_decoded {:?}",
+                temp_translation_map2.get_by_left(out_segment.as_str()) /*.unwrap_or_else(||&"")*/
+            );
+            decode_string_temp.push_str(
+                temp_translation_map2
+                    .get_by_left(out_segment.as_str())
+                    .unwrap_or_else(|| &""),
+            );
+            // decode_string_temp.push_str(temp_translation_map2.get_by_left(out_segment.as_str()).unwrap_or_else(||&""));
+        }
+        println!("output {}", decode_string_temp);
+        total_sum += decode_string_temp.parse::<usize>().unwrap_or(0);
     }
-    println!("charcount {}", charcount);
     // Ok(charcount as i128)
-    Ok(0)
+    Ok(total_sum as i128)
 }
